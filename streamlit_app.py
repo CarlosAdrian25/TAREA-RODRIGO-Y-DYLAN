e885e840253d3b399d62a06c65aa4373702b65e3
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-06-27 04:05:06 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,11 @@ st.sidebar.title("MENU DE OPCIONES")
 selected = st.sidebar.selectbox("SELECCIONA UNA OPCION", ["Total de ventas", "Total ventas cliente","Ventas por Dia","Ventas por Fecha"])
 
 if selected == "Total de ventas":
-    st.title("Bienvenido a la pagina de ventas")
+    where_to= st.sidebar.selectbox("Total de ventas:", ["Unidades Factura", "Valor por Factura"])
+    if where_to == "Unidades Factura":
+        st.title("Total de ventas por Unidades Factura")
+    elif where_to=="Valor por Factura":
+        st.title("Total de ventas por Valor por Factura")
 elif selected == "Total ventas cliente":
     st.title("Bienvenido a la pagina de ventas por cliente")
 elif selected=="Ventas por Dia":
