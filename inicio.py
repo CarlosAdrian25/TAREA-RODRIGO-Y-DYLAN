10b32aaedbe91741ba9ce20891cb86d3f861242e
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-06-28 05:13:02 +0000

    commit

--- inicio.py
+++ inicio.py
@@ -21,12 +21,4 @@ def mostrar():
     # Mostrar gráfico en Streamlit
     st.pyplot(fig)
 
-def mostrar_grafico_pie():
-    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-    sizes = [15, 30, 45, 10]
-    explode = (0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    st.pyplot(fig1)
+
