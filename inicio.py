49a9fe5bd270e3cea9be902c9e48c7eb2cf2e309
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-06-28 04:33:37 +0000

    commit

--- inicio.py
+++ inicio.py
@@ -1,18 +1,25 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import numpy as np
 
 def mostrar():
+    # Cargar datos desde el archivo CSV
     file_path = 'data/ventas_traducido_completo.csv'
     ventas_df = pd.read_csv(file_path)
-    columnas = ventas_df.columns
+    
     # Agrupar datos por 'Descripcion' y sumar la 'Cantidad'
     data = ventas_df.groupby('Descripcion')['Cantidad'].sum()
-    # Crear gráfico de pie
+    
+    # Preparar datos para etiquetas y tamaños de secciones del gráfico de pastel
+    labels = data.index  # Usamos los índices de 'data' como etiquetas
+    sizes = data.values  # Usamos los valores de 'data' como tamaños
+    
+    # Crear gráfico de pastel
     fig, ax = plt.subplots()
-    ax.pie(data, labels=columnas.index, autopct='%1.1f%%', shadow=True,startangle=90)
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
     ax.axis('equal')  # Para asegurar que el gráfico es un círculo.
-
+    
     # Mostrar gráfico en Streamlit
     st.pyplot(fig)
 
