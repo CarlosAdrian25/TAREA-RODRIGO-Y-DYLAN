2824586b665e68a423b5776eef1782a447682a58
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-06-28 04:03:24 +0000

    commit

--- inicio.py
+++ inicio.py
@@ -5,8 +5,17 @@ import matplotlib.pyplot as plt
 def mostrar():
     file_path = 'data/ventas_traducido_completo.csv'
     ventas_df = pd.read_csv(file_path)
-    st.write(ventas_df)
-    st.write(ventas_df.shape)
+    ventas_df = pd.read_csv(file_path)
+    # Agrupar datos por 'Descripcion' y sumar la 'Cantidad'
+    data = ventas_df.groupby('Descripcion')['Cantidad'].sum()
+    
+    # Crear gráfico de pie
+    fig, ax = plt.subplots()
+    ax.pie(data, labels=data.index, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Para asegurar que el gráfico es un círculo.
+
+    # Mostrar gráfico en Streamlit
+    st.pyplot(fig)
 
 def mostrar_grafico_pie():
     file_path = 'data/ventas_traducido_completo.csv'
