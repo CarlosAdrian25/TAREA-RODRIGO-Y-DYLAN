46864e820b42d7198ae19e413982e8b2d301f9bf
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-07-01 02:26:18 +0000

    Pending changes exported from your codespace

--- inicio.py
+++ inicio.py
@@ -2,66 +2,156 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
-import pandas as pd
-import streamlit as st
-import matplotlib.pyplot as plt
-
 def mostrar():
     file_path = 'data/ventas_traducido.csv'
     ventas_df = pd.read_csv(file_path)
     st.write(ventas_df)
     st.write(ventas_df.shape)
 
-def total_ventas_por_producto():
+def total_ventas_por_factura():
     file_path = 'data/ventas_traducido.csv'
     ventas_df = pd.read_csv(file_path)
-    total_ventas = ventas_df.groupby('Descripcion')['Cantidad'].sum().reset_index()
-    total_ventas.columns = ['Descripcion', 'TotalVentas']
-    st.write(total_ventas)
+    
+    
+    total_ventas = ventas_df.groupby('NoFactura')['Cantidad'].sum().reset_index()
+    total_ventas.columns = ['NumeroFactura', 'TotalVentas']
+    
+    
+    total_ventas['NumeroFactura'] = total_ventas['NumeroFactura'].astype(str)
+    
+   
+    total_ventas_10 = total_ventas.head(10)
+   
+    plt.figure(figsize=(10, 6))
+    plt.bar(total_ventas_10['NumeroFactura'], total_ventas_10['TotalVentas'], edgecolor='k', alpha=0.7)
+    plt.title('Total de Ventas por Factura (Top 10)')
+    plt.xlabel('Número de Factura')
+    plt.ylabel('Cantidad Vendida')
+    plt.xticks(rotation=90) 
+    plt.grid(True)
+    
+    
+    st.pyplot(plt)
 
-def total_ventas_por_CLIENTE():
+def total_ventas_por_cliente():
     file_path = 'data/ventas_traducido.csv'
     ventas_df = pd.read_csv(file_path)
+    
+    
     total_ventas = ventas_df.groupby('IdCliente')['Cantidad'].sum().reset_index()
-    total_ventas.columns = ['Descripcion', 'TotalVentas']
-    st.title('Nº DE VENTAS POR CLIENTE')
-    st.write(total_ventas)
+    total_ventas.columns = ['IdCliente', 'TotalVentas']
+    
+  
+    total_ventas['IdCliente'] = total_ventas['IdCliente'].astype(str)
+    
+    
+    total_ventas_10 = total_ventas.head(10)
+    st.title('Nº DE VENTAS POR CLIENTE (Top 10)')
+    # st.write(total_ventas_10)
+    # st.write(total_ventas_10.shape)
+    
+    plt.figure(figsize=(10, 6))
+    plt.bar(total_ventas_10['IdCliente'], total_ventas_10['TotalVentas'],color='green', edgecolor='k', alpha=0.7)
+    plt.title('NUMERO DE VENTAS POR CLIENTE')
+    plt.xlabel('ID de Cliente')
+    plt.ylabel('Cantidad Vendida')
+    plt.xticks(rotation=90)  
+    plt.grid(True)
+    
+    
+    st.pyplot(plt)
 
 def total_Factura():
     file_path = 'data/ventas_traducido.csv'
     ventas_df = pd.read_csv(file_path)
     Cantidad = ventas_df['NoFactura'].value_counts().reset_index()
     Cantidad.columns = ['NoFactura', 'Cantidad']
-    st.title('TOTAL DE VENTAS (UNIDADES) FACTURA')
+    
     st.write(Cantidad)
 
-def total_ventas_por_Fecha():
+def total_ventas_por_fecha():
     file_path = 'data/ventas_traducido.csv'
     ventas_df = pd.read_csv(file_path)
+    
+   
     total_ventas = ventas_df.groupby('Fecha')['Cantidad'].sum().reset_index()
-    total_ventas.columns = ['Descripcion', 'TotalVentas']
-    st.title('CANTIDAD DE VENTAS POR FECHA')
-    st.write(total_ventas)
+    total_ventas.columns = ['Fecha', 'TotalVentas']
+    
+    
+    total_ventas_10 = total_ventas.head(10)
+    st.title('CANTIDAD DE VENTAS POR FECHA (Top 10)')
+    st.write(total_ventas_10)
+    st.write(total_ventas_10.shape)
+    
+    
+    plt.figure(figsize=(10, 6))
+    plt.bar(total_ventas_10['Fecha'], total_ventas_10['TotalVentas'], color='skyblue', edgecolor='k', alpha=0.7)
+    plt.title('Cantidad de Ventas por Fecha (Top 10)')
+    plt.xlabel('Fecha')
+    plt.ylabel('Cantidad Vendida')
+    plt.xticks(rotation=90) 
+    plt.grid(True)
+    
+   
+    st.pyplot(plt)
 
 def total_ventas_por_dia():
     file_path = 'data/ventas_traducido_completo.csv'
     ventas_df = pd.read_csv(file_path)
+    
+    
     ventas_df['Fecha'] = pd.to_datetime(ventas_df['Fecha'])
+    
+    
     ventas_df['Dia'] = ventas_df['Fecha'].dt.day
+    
+   
     total_ventas = ventas_df.groupby('Dia')['Cantidad'].sum().reset_index()
     total_ventas.columns = ['Dia', 'TotalVentas']
-    st.title('CANTIDAD DE VENTAS POR DÍA')
-    st.write(total_ventas)
+    
+   
+    total_ventas_10 = total_ventas.head(10)
+    st.title('CANTIDAD DE VENTAS POR DÍA (Top 10)')
+    st.write(total_ventas_10)
+    st.write(total_ventas_10.shape)
+    
+    
+    plt.figure(figsize=(10, 6))
+    plt.bar(total_ventas_10['Dia'], total_ventas_10['TotalVentas'], color='skyblue', edgecolor='k', alpha=0.7)
+    plt.title('Cantidad de Ventas por Día (Top 10)')
+    plt.xlabel('Día del Mes')
+    plt.ylabel('Cantidad Vendida')
+    plt.xticks(rotation=90)  
+    plt.grid(True)
+    
+   
+    st.pyplot(plt)
 
 def total():
     file_path = 'data/ventas_traducido.csv'
     ventas_df = pd.read_csv(file_path)
     
-    # Agrupar por 'NoFactura' y calcular la suma de 'Total' para cada factura
-    total_por_factura = ventas_df.groupby('NoFactura').agg({'Total': 'sum'}).reset_index()
+    total_por_factura_10 = ventas_df[['NoFactura', 'Total']].head(10)
     
-    # Mostrar el título utilizando st.title si estás usando Streamlit
-    st.title('TOTAL DE VENTAS POR FACTURA')
+    st.title('TOTAL DE VENTAS POR FACTURA (Top 10)')
     
-    # Mostrar los resultados utilizando st.write
-    st.write(total_por_factura)
+    st.write(total_por_factura_10)
+    st.write(total_por_factura_10.shape)
+    
+    plt.figure(figsize=(10, 6))
+    
+    numeros_factura = total_por_factura_10['NoFactura'].unique()
+    
+    # Graficar las barras
+    plt.bar(numeros_factura, 
+            total_por_factura_10.groupby('NoFactura')['Total'].sum(),  
+            edgecolor='orange',    
+            alpha=0.7,
+            linewidth=9)      
+    plt.title('Total de Ventas por Factura (Top 10)')
+    plt.xlabel('Número de Factura')
+    plt.ylabel('Total Vendido')
+    plt.xticks(rotation=90)  
+    plt.grid(True)
+
+    st.pyplot(plt)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,36 @@
 import streamlit as st
-from inicio import mostrar,total_ventas_por_dia,total_Factura,total_ventas_por_Fecha,total_ventas_por_CLIENTE,total
+from inicio import mostrar,total_ventas_por_dia,total_Factura,total_ventas_por_fecha,total_ventas_por_cliente,total,total_ventas_por_factura
 from total_ventas_cliente import mostrar_py
 
 st.sidebar.title("MENU DE OPCIONES")
 selected = st.sidebar.selectbox("SELECCIONA UNA OPCION", ["INICIO", "Total de ventas", "Total ventas cliente", "Ventas por Dia", "Ventas por Fecha"])
 
 if selected == "INICIO":
-    mostrar()
-    # total_ventas_por_producto()
-    total_Factura()
-    total_ventas_por_CLIENTE()
-    total_ventas_por_Fecha()
-    total_ventas_por_dia()
-    total()
+    # mostrar()
+    col1,col2,col3,col4,col5 = st.beta_columns(5)
+    with col1:
+        total_ventas_por_factura()
+    with col2:
+       total_ventas_por_cliente()
+        
+    with col3:
+        total_ventas_por_fecha()
+        
+    with col4:
+        total_ventas_por_dia()
+        
+    with col5:
+        total()
 elif selected == "Total de ventas":
     where_to = st.sidebar.selectbox("Total de ventas:", ["Unidades Factura", "Valor por Factura"])
     if where_to == "Unidades Factura":
         st.title("Unidades Factura")
-        mostrar_py()
+        col1, col2 = st.columns(2)  # Crear 2 columnas
+        with col1:
+           mostrar_py()
+        with col2:
+         st.write(" ")
+         total_Factura()
     elif where_to == "Valor por Factura":
         st.title("Valor por Factura")
 elif selected == "Total ventas cliente":
